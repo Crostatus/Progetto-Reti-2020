6b0b3d7ffea28ebd6b60d4985551726380b9c19e
Andrea <andreaboccone33@gmail.com>
2020-01-10 18:08:39 +0100

    Add files via upload

--- MainClient.java
+++ MainClient.java
@@ -1,15 +1,16 @@
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+
 
 public class MainClient {
     public static void main(String[] args) throws Exception, NotBoundException {
         Registry reg = LocateRegistry.getRegistry(8080);
         RegistrazioneInterface registra = (RegistrazioneInterface) reg.lookup("iscrizione");
-        int risultato=registra.registra_utente("bastianich", "puppasedani69");
+        int risultato=registra.registra_utente("bastianich11", "ciaociao98");
         if(risultato==0)
             System.out.println("Operazione avvenuta con successo");
         else if(risultato==1)
@@ -17,5 +18,45 @@ public class MainClient {
         else
             System.out.println("Password non valida, errore: "+risultato);
 
+        InetSocketAddress address = new InetSocketAddress("localhost", 6666);
+        SocketChannel client = SocketChannel.open(address);
+        ByteBuffer buffer = ByteBuffer.allocate(512);
+        String messaggio = "login bastianich11 ciaociao98";
+        byte[] data = messaggio.getBytes();
+        buffer.put(data);
+        buffer.flip();
+        while(buffer.hasRemaining())
+            client.write(buffer);
+        System.out.println("Ho inviato: " + messaggio);
+        buffer.clear();
+        client.close();
+
+
     }
+
+
+    /*InetSocketAddress address = new InetSocketAddress(hostName,port);
+        SocketChannel client = SocketChannel.open(address);
+        ByteBuffer buffer=ByteBuffer.allocateDirect(256);
+
+        // Mando il messaggio al server
+        buffer.put(data.getBytes());
+        buffer.flip();
+        while(buffer.hasRemaining())
+            client.write(buffer);
+        System.out.println("Ho inviato: "+data);
+        buffer.clear();
+        Thread.sleep(100);
+
+        // Leggo dal server
+        int num=client.read(buffer);
+        byte []data=new byte[num];
+        buffer.flip();
+        buffer.get(data);
+        System.out.println("Ho ricevuto: "+new String(data));
+        buffer.clear();
+        client.close();
+        */
+
+
 }
---
+++ ReadingByteBuffer.java
@@ -0,0 +1,50 @@
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
+public class ReadingByteBuffer {
+    private ByteBuffer byteBuffer;
+    private String message;
+
+    public ReadingByteBuffer() {
+        byteBuffer = ByteBuffer.allocate(64);
+        message = "";
+    }
+/*
+    public boolean updateOnRead() {
+        byteBuffer.flip();
+        int bytesNum = byteBuffer.limit();
+        byte[] data = new byte[bytesNum];
+        byteBuffer.get(data);
+        String result = new String(data);
+        message += result;
+        byteBuffer.clear();
+
+        if (message.endsWith("_EOM")) {
+            message = message.replace("_EOM", " - Echoed by the server");
+            byteBuffer = Charset.forName("ISO-8859-1").encode(CharBuffer.wrap(message.toCharArray()));
+            return true;
+        }
+        return false;
+    }
+*/
+
+    public void updateOnRead() {
+        byteBuffer.flip();
+        int bytesNum = byteBuffer.limit();
+        byte[] data = new byte[bytesNum];
+        byteBuffer.get(data);
+        String result = new String(data);
+        message += result;
+        byteBuffer.clear();
+    }
+
+
+    public String getMessage(){
+        return message;
+    }
+
+    public ByteBuffer getByteBuffer() {
+        return byteBuffer;
+    }
+}
