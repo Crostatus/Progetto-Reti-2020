6fbf5a81ce47c7a6373a6f6aa8ab3108af2ea374
Andrea <andreaboccone33@gmail.com>
2020-02-08 10:17:32 +0100

    Delete RegistrazioneInterface.java

--- RegistrazioneInterface.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RegistrazioneInterface  extends Remote {
-
-    int registra_utente(String nickname, String password) throws IOException;
-}
